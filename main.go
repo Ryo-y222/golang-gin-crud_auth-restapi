dc0fcb3dea60208008a59867dae7908fa9efba25
Ryo-y222 <right_hand9@yahoo.co.jp>
2025-06-30 03:24:21 +0900

    add-database and gitignore-docker

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"gin-fleamarket/controllers"
-	"gin-fleamarket/models"
+	"gin-fleamarket/infra"
+
+	// "gin-fleamarket/models"
 	"gin-fleamarket/repositories"
 	"gin-fleamarket/services"
 
@@ -10,22 +12,34 @@ import (
 )
 
 func main() {
+	infra.Initialize()
+	// log.Println(os.Getenv("ENV"))
+	db := infra.SetupDB()
 
-	items := []models.Item{
-		{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
-		{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
-		{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
-	}
+	// items := []models.Item{
+	// 	{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
+	// 	{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
+	// 	{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
+	// }
 
-	itemRepository := repositories.NewItemMemoryRepository(items)
+	// itemRepository := repositories.NewItemMemoryRepository(items)
+	itemRepository := repositories.NewItemRepository(db)
 	itemService := services.NewItemService(itemRepository)
 	itemController := controllers.NewItemController(itemService)
 
+	authRepository := repositories.NewAuthRepository(db)
+	authService := services.NewAuthService(authRepository)
+	authController := controllers.NewAuthController(authService)
+
 	r := gin.Default()
-	r.GET("/items", itemController.FindAll)
-	r.GET("/items/:id", itemController.FindById)
-	r.POST("items/", itemController.Create)
-	r.PUT("items/:id", itemController.Update)
-	r.DELETE("items/:id", itemController.Delete)
+	authRouter := r.Group("/auth")
+	itemRouter := r.Group("items")
+
+	itemRouter.GET("", itemController.FindAll)
+	itemRouter.GET("/:id", itemController.FindById)
+	itemRouter.POST("", itemController.Create)
+	itemRouter.PUT(":id", itemController.Update)
+	itemRouter.DELETE(":id", itemController.Delete)
+	authRouter.POST("/signup", authController.Signup)
 	r.Run("localhost:8080")
 }
